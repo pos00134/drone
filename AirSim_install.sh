e5803588a793f9cb93f8551b6082ad48e0ad8311
CuriousBearH <77527414+CuriousBearH@users.noreply.github.com>
2021-07-17 15:46:13 +0900

    Update AirSim_install.sh

--- AirSim_install.sh
+++ AirSim_install.sh
@@ -7,7 +7,9 @@ cd AirSim
 
 sudo apt-get install gcc-8 g++-8
 sudo apt-get install ros-melodic-tf2-sensor-msgs ros-melodic-tf2-geometry-msgs ros-melodic-mavros*
+
 cd ros
+source /opt/ros/melodic/setup.bash
 catkin build -DCMAKE_C_COMPILER=gcc-8 -DCMAKE_CXX_COMPILER=g++-8
 
 echo "source ~/AirSim/ros/devel/setup.bash" >> ~/.bashrc
